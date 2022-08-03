147bdf76813169f466a85acf2dced4d874e190fe
Abdirahim Hussein <drahimhussein@gmail.com>
2022-08-03 17:50:27 +0100

    completed floats and function in quick session

--- exercises/K-functions-parameters/exercise5.js
+++ exercises/K-functions-parameters/exercise5.js
@@ -1,5 +1,9 @@
 // Declare your function here
-
+function Age(Age){
+    return 30;
+}
+function createLongGreeting(name, Age){
+    return "Daniel" , 30
+}
 const greeting = createLongGreeting("Daniel", 30);
-
 console.log(greeting);
