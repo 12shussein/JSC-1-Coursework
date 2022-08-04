c9eb2b51c0a3f46a09dbb0b1ba2f565e0f69d5ca
Abdirahim Hussein <drahimhussein@gmail.com>
2022-08-04 17:56:38 +0100

    completed another task with explations of the function

--- mandatory/3-function-output.js
+++ mandatory/3-function-output.js
@@ -1,14 +1,21 @@
 // Add comments to explain what this function does. You're meant to use Google!
+/* The Math.random() function returns a floating-point, pseudo-random number in the range 
+0 to less than 1 (inclusive of 0, but not 1) with approximately uniform distribution over that range — 
+which you can then scale to your desired range */
 function getRandomNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+/*The concat() method concatenates the string arguments to the calling string and returns a new string
+in this instance a function is declared with two strings inside the parameters which returns using the concat() function*/
+
 function combine2Words(word1, word2) {
   return word1.concat(word2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
+  return firstWord.concat(" ", secondWord, " ", thirdWord);
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
 }
